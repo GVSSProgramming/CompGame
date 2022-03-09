003ecc4e2aafd83ddde241f3a0873b8985bf3959
Charan <dechair3@gmail.com>
2022-03-08 21:57:38 -0500

    misc: website interface wip

--- index.js
+++ index.js
@@ -14,24 +14,20 @@ class game {
         this.enemies[0] = new enemy();
         this.enemies[1] = new enemy();
         for (let i = 0; i < 100; i++) {
-            console.log(`turn ${i}`);
-            this.player.onTurn();
-            this.onTurnFunc(this);
-
             var EnName_1 = document.getElementById('EnName_1')
-            var name = webGameObj.getEnemies()[0].getName();
+            var name = gameObj.getEnemies()[0].getName();
             EnName_1.innerHTML = name
             
             var EnHealth_1 = document.getElementById('EnHealth_1')
-            var health = webGameObj.getEnemies()[0].getHp();
+            var health = gameObj.getEnemies()[0].getHp();
             EnHealth_1.innerHTML = health
 
             var EnName_2 = document.getElementById('EnName_2')
-            var name = webGameObj.getEnemies()[1].getName();
+            var name = gameObj.getEnemies()[1].getName();
             EnName_2.innerHTML = name
 
             var EnHealth_2 = document.getElementById('EnHealth_2')
-            var health = webGameObj.getEnemies()[1].getHp();
+            var health = gameObj.getEnemies()[1].getHp();
             EnHealth_2.innerHTML = health
 
             var turn = document.getElementById('turn_id')
@@ -39,6 +35,14 @@ class game {
             
             var kill_id = document.getElementById('kill_id')
             kill_id.innerHTML = (gameObj.getPlayer().getKills())
+
+            var move_id = document.getElementById('move_id')
+            move_id.innerHTML = 3 - gameObj.getPlayer().getMoves();
+
+            console.log(`turn ${i}`);
+            this.player.onTurn();
+            this.onTurnFunc(this);
+            
         }
     }
     getPlayer() {
@@ -50,13 +54,13 @@ class game {
 }
 const func = function (gameObj) {
     //for (let i = 0; i < 2; i++){
-        console.log(gameObj.getPlayer().getMoves());
+       /* console.log(gameObj.getPlayer().getMoves());
         console.log(`Attack: ${gameObj.getPlayer().attack(gameObj.getEnemies())}`);
         console.log(`Attack: ${gameObj.getPlayer().attack(gameObj.getEnemies())}`);
         console.log(`Spell: ${gameObj.getPlayer().castSpell(3, 25, gameObj.getEnemies())}`);
-        console.log(`Attack: ${gameObj.getPlayer().attack(gameObj.getEnemies())}`);
+        console.log(`Attack: ${gameObj.getPlayer().attack(gameObj.getEnemies())}`); */
+        while((gameObj.getPlayer().getMoves()) < 3){}
        // }
 
 }
 const gameObj = new game("shin", func);
-gameObj.start();
